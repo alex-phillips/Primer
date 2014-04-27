271689a82dfb7de1c4b95079894c104c5f471189
exonintrendo <exonintrendo@gmail.com>
2014-04-27 01:21:30 -0400

    removed unnecessary routes, added one for movies

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -8,8 +8,7 @@
 
 Router::route('/', array('controller' => 'posts', 'action' => 'index'));
 Router::route('/user/:username', array('controller' => 'users', 'action' => 'view', 'username'));
-Router::route('/first', array('controller' => 'posts', 'action' => 'view', "1"));
 Router::route('/login/', array('controller' => 'users', 'action' => 'login'));
 Router::route('/logout/', array('controller' => 'users', 'action' => 'logout'));
 Router::route('/register/', array('controller' => 'users', 'action' => 'add'));
-Router::route('/user/*', array('controller' => 'users', 'action' => 'view'));
+Router::route('/movies', array('controller' => 'pages', 'action' => 'movies'));
