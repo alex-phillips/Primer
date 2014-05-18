fe6c7c8bba0c346aa2d302f798540f29b1b8e75f
exonintrendo <exonintrendo@gmail.com>
2014-05-18 15:05:22 -0400

    added setAttribute to the Database class rather than Model

--- Primer/Core/Database.php
+++ Primer/Core/Database.php
@@ -13,5 +13,6 @@ class Database extends PDO
         // generate a database connection, using the PDO connector
         // @see http://net.tutsplus.com/tutorials/php/why-you-should-be-using-phps-pdo-for-database-access/
         parent::__construct(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS, $options);
+        $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 }
