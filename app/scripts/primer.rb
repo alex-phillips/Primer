b063c5e4994db26a313357ec2aa9d178bedd394f
exonintrendo <exonintrendo@gmail.com>
2014-05-25 11:02:28 -0400

    apparently using class variables in ruby is bad practice - adds to global namespace? might come back to this...

--- app/scripts/primer.rb
+++ app/scripts/primer.rb
@@ -2,14 +2,14 @@ require 'logger'
 
 class Primer
 
-  @@log_directory = '../logs/'
+  @log_directory = '../logs/'
 
   def self.log_message (file, message, level='info')
-    if !defined? @@logger
-      @@logger = Logger.new(@@log_directory + file)
-      @@logger.level = Logger::DEBUG
+    if !defined? @logger
+      @logger = Logger.new(@log_directory + file)
+      @logger.level = Logger::DEBUG
     end
-    @@logger.send(level, message)
+    @logger.send(level, message)
   end
 
 end
