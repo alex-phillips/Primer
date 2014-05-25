8dc673457207058c93964b0ec9c871ddd3f4214d
exonintrendo <exonintrendo@gmail.com>
2014-05-25 11:08:28 -0400

    reek warns against parens around conditionals - just bad practice?

--- app/scripts/ip_checker.rb
+++ app/scripts/ip_checker.rb
@@ -10,7 +10,7 @@ class IPChecker
     active_ip = Net::HTTP.get(URI.parse('http://ipecho.net/plain'))
     self.log_message('Active IP is ' + active_ip)
 
-    if (File.exists?(@monitor_file))
+    if File.exists?(@monitor_file)
         recorded_ip = File.read(@monitor_file)
         self.log_message('Recorded IP is ' + recorded_ip.gsub("\n",''))
     else
@@ -18,7 +18,7 @@ class IPChecker
         self.log_message('No recorded IP.')
     end
 
-    if (active_ip.eql? recorded_ip)
+    if active_ip.eql? recorded_ip
         self.log_message('No IP change')
     else
         self.log_message('IP address has changed')
