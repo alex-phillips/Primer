1998a178dda51b3f04ea72b615e05035872f5211
exonintrendo <exonintrendo@gmail.com>
2014-06-01 12:13:59 -0400

    added function to get model name of passed in string, added function to determine if an array is hashed or not

--- Primer/Core/Primer.php
+++ Primer/Core/Primer.php
@@ -228,4 +228,19 @@ class Primer
     {
         return ucfirst(Inflector::pluralize($string) . 'Controller');
     }
+
+    public static function getModelName($string)
+    {
+        return ucfirst(Inflector::singularize($string));
+    }
+
+    public static function isHashedArray(array $array)
+    {
+        // Keys of the array
+        $keys = array_keys($array);
+
+        // If the array keys of the keys match the keys, then the array must
+        // not be associative (e.g. the keys array looked like {0:0, 1:1...}).
+        return array_keys($keys) !== $keys;
+    }
 }
