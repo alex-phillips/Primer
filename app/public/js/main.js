16d1751d4dbdb5bd7d504f64eb7865cdaa0730a1
exonintrendo <exonintrendo@gmail.com>
2014-06-02 21:08:28 -0400

    added underscore JS

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -2,6 +2,7 @@ require.config({
     paths: {
         'jquery': '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
         'bootstrap': '//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min',
+        'underscore': '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min',
         'mmenu': 'vendor/jquery.mmenu/jquery.mmenu',
         'modernizr': 'vendor/modernizr',
         'angular': '//ajax.googleapis.com/ajax/libs/angularjs/1.2.12/angular.min',
