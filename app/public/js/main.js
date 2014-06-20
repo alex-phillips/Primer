9048a0f9404b310d802b8f58103b7a2232c63aff
exonintrendo <exonintrendo@gmail.com>
2014-06-19 22:01:29 -0400

    removed ckeditor, angular, and slimscroll

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,31 +1,27 @@
 require.config({
     paths: {
         'jquery': '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
+        'jquery-ui': '//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min',
         'bootstrap': '//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min',
         'underscore': '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min',
         'mmenu': 'vendor/jquery.mmenu/jquery.mmenu',
         'modernizr': 'vendor/modernizr',
-        'angular': '//ajax.googleapis.com/ajax/libs/angularjs/1.2.12/angular.min',
-        'ckeditor': '../libs/ckeditor/ckeditor',
-        'ckeditor-jquery': '../libs/ckeditor/adapters/jquery',
         'prism': 'vendor/prism'
     },
     shim: {
         'jquery': {
             exports: '$'
         },
+        'jquery-ui': {
+            deps: ['jquery']
+        },
         'bootstrap': {
             deps: ['jquery']
         },
         'mmenu': {
             deps: ['jquery']
-        },
-        'angular': {
-            exports: 'angular'
-        },
-        'ckeditor-jquery': {
-            deps: ['ckeditor']
         }
     },
+    enforeDefine: true,
     deps: ['app']
 });
