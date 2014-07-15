9afe122698355423f8f41f1c8dff619b863edc7b
exonintrendo <exonintrendo@gmail.com>
2014-07-15 19:14:45 -0400

    using a DATABASE_CONFIG class now instead of using DEFINEs in one giant config file. Can also control the 'environment' you're in

--- Primer/Core/Database.php
+++ Primer/Core/Database.php
@@ -4,15 +4,20 @@ class Database extends PDO
 {
     public function __construct()
     {
-        // set the (optional) options of the PDO connection. in this case, we set the fetch mode to
-        // "objects", which means all results will be objects, like this: $result->user_name !
-        // For example, fetch mode FETCH_ASSOC would return results like this: $result["user_name] !
-        // @see http://www.php.net/manual/en/pdostatement.fetch.php
-        $options = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ);
+        $config = new DATABASE_CONFIG();
+        $config = $config->{ENVIRONMENT};
 
-        // generate a database connection, using the PDO connector
-        // @see http://net.tutsplus.com/tutorials/php/why-you-should-be-using-phps-pdo-for-database-access/
-        parent::__construct(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS, $options);
-        $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        if (strcasecmp($config['db_type'], 'mysql') === 0) {
+            // set the (optional) options of the PDO connection. in this case, we set the fetch mode to
+            // "objects", which means all results will be objects, like this: $result->user_name !
+            // For example, fetch mode FETCH_ASSOC would return results like this: $result["user_name] !
+            // @see http://www.php.net/manual/en/pdostatement.fetch.php
+            $options = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ);
+
+            // generate a database connection, using the PDO connector
+            // @see http://net.tutsplus.com/tutorials/php/why-you-should-be-using-phps-pdo-for-database-access/
+            parent::__construct($config['db_type'] . ':host=' . $config['host'] . ';dbname=' . $config['database'], $config['login'], $config['password'], $options);
+            $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
     }
 }
