b20a548773c5730cea5e2da65ee56cb0952dabb9
exonintrendo <exonintrendo@gmail.com>
2014-08-03 13:41:11 -0400

    removed angular

--- app/public/js/angularjs/routes.js
+++
@@ -1,9 +0,0 @@
-//define(['./ng_app'], function (app) {
-//    'use strict';
-//    return app.config(['$routeProvider', function ($routeProvider) {
-//        $routeProvider.when('/pages/movies/', {
-//            templateUrl: '/public/angularjs/templates/movie_list.html',
-//            controller: 'moviesController'
-//        });
-//    }]);
-//});
