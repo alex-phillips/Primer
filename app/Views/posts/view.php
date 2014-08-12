65cd550c2ed87a3d267cf8f6893924f599b305c5
exonintrendo <exonintrendo@gmail.com>
2014-08-11 22:15:49 -0400

    escaping code except those in <code> tag

--- app/Views/posts/view.php
+++ app/Views/posts/view.php
@@ -2,6 +2,9 @@
 
 $markdownParser = new Parsedown();
 $this->post->body = $markdownParser->text(htmlspecialchars($this->post->body, ENT_QUOTES, 'utf-8'));
+$this->post->body = preg_replace_callback('#\<code(.+?)\>(.+?)\<\/code\>#s', function ($matches) {
+    return '<code' . $matches[1] . '>' . htmlspecialchars_decode($matches[2]) . '</code>';
+}, $this->post->body);
 
 $markup = <<<___HTML___
 <p>{$this->post->body}</p>
