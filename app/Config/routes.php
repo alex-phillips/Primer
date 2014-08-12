0f2fc19349efefe3cbccdefde51b1131ab7e0068
exonintrendo <exonintrendo@gmail.com>
2014-08-11 22:00:27 -0400

    added route for post and apis

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -8,7 +8,9 @@
 
 Router::route('/', array('controller' => 'posts', 'action' => 'index'));
 Router::route('/user/:username', array('controller' => 'users', 'action' => 'view', 'username'));
+Router::route('/post/:slug', array('controller' => 'posts', 'action' => 'view', 'slug'));
 Router::route('/login/', array('controller' => 'users', 'action' => 'login'));
 Router::route('/logout/', array('controller' => 'users', 'action' => 'logout'));
 Router::route('/register/', array('controller' => 'users', 'action' => 'add'));
 Router::route('/movies', array('controller' => 'pages', 'action' => 'movies'));
+Router::route('/api/', array('controller' => 'apis', 'action' => 'index'));
