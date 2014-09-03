48a7321885a6fc91b63c09e05bd2cc5e89af6107
exonintrendo <exonintrendo@gmail.com>
2014-09-03 13:05:05 -0400

    fixed bug regarding missing hash factor variable - moved from auth component

--- Primer/lib/Primer/Utility/Security.php
+++ Primer/lib/Primer/Utility/Security.php
@@ -25,6 +25,29 @@ class Security
     private $_session;
     private $_request;
 
+    /*
+     * Configuration for: Hashing strength
+     * This is the place where you define the strength of your password hashing/salting
+     *
+     * To make password encryption very safe and future-proof, the PHP 5.5 hashing/salting functions
+     * come with a clever so called COST FACTOR. This number defines the base-2 logarithm of the rounds of hashing,
+     * something like 2^12 if your cost factor is 12. By the way, 2^12 would be 4096 rounds of hashing, doubling the
+     * round with each increase of the cost factor and therefore doubling the CPU power it needs.
+     * Currently, in 2013, the developers of this functions have chosen a cost factor of 10, which fits most standard
+     * server setups. When time goes by and server power becomes much more powerful, it might be useful to increase
+     * the cost factor, to make the password hashing one step more secure. Have a look here
+     * (@see https://github.com/panique/php-users/wiki/Which-hashing-&-salting-algorithm-should-be-used-%3F)
+     * in the BLOWFISH benchmark table to get an idea how this factor behaves. For most people this is irrelevant,
+     * but after some years this might be very very useful to keep the encryption of your database up to date.
+     *
+     * Remember: Every time a user registers or tries to log in (!) this calculation will be done.
+     * Don't change this if you don't know what you do.
+     *
+     * To get more information about the best cost factor please have a look here
+     * @see http://stackoverflow.com/q/4443476/1114320
+     */
+    private $_hashCostFactor = 10;
+
     public function __construct(
         SessionComponent $session,
         RequestComponent $request
