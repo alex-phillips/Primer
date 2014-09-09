667f0b587ba00fa8c974eb511dcda1b0689d80d6
exonintrendo <exonintrendo@gmail.com>
2014-09-09 19:11:25 -0400

    no need to get database config here anymore

--- app/public/index.php
+++ app/public/index.php
@@ -16,8 +16,6 @@ else {
     require_once(ROOT . '/app/Config/config.php');
 }
 
-require_once(ROOT . DS . 'app' . DS . 'Config' . DS . 'database.php');
-
 // checking for minimum PHP version
 if (version_compare(PHP_VERSION, '5.3.7', '<') ) {
     exit("Sorry, this framework does not run on a PHP version smaller than 5.3.7!");
