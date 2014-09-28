b5a9bb9a94808856938b33882613f8750d176c5b
exonintrendo <exonintrendo@gmail.com>
2014-09-28 10:39:32 -0400

    added 'record' function to proxy class to allow a method of creating a new channel/log file for different scripts, controllers, wherever. This way, not everything HAS to be logged to the same file.

--- lib/Primer/Proxy/Log.php
+++ lib/Primer/Proxy/Log.php
@@ -8,10 +8,33 @@
 
 namespace Primer\Proxy;
 
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+
 class Log extends Proxy
 {
+    private static $_channels = array();
+
     protected static function getProxyAccessor()
     {
         return 'logger';
+    }
+
+    public static function record($message, $file, $level = 'info', $context = array())
+    {
+        if (!array_key_exists($file, self::$_channels)) {
+            $logger = new Logger($file);
+            if (static::$_app['config']['app.log_daily_files'] === true) {
+                $logger->pushHandler(new RotatingFileHandler(LOG_PATH . $file, 7));
+            }
+            else {
+                $logger->pushHandler(new StreamHandler(LOG_PATH . $file));
+            }
+
+            self::$_channels[$file] = $logger;
+        }
+
+        self::$_channels[$file]->$level($message, $context);
     }
 }
