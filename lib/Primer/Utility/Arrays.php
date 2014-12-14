85306af3c749f1d88e019fe2556dd42b03700a45
exonintrendo <exonintrendo@gmail.com>
2014-12-14 13:47:44 -0500

    added function to determine if an array is a hashed array

--- lib/Primer/Utility/Arrays.php
+++ lib/Primer/Utility/Arrays.php
@@ -799,4 +799,14 @@ class Arrays
         }
         return $count;
     }
+
+    public static function isHashedArray(array $array)
+    {
+        // Keys of the array
+        $keys = array_keys($array);
+
+        // If the array keys of the keys match the keys, then the array must
+        // not be associative (e.g. the keys array looked like {0:0, 1:1...}).
+        return array_keys($keys) !== $keys;
+    }
 }
